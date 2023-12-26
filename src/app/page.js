2ca5fd20213760b5ef235b02db2aebd493fd723b
Abhinaya Radiansyah Listiyanto <114990549+Abhinaya173@users.noreply.github.com>
2023-12-26 11:30:58 +0700

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import Footer from 'src/app/footer/footer'
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -205,6 +206,7 @@ export default function Home() {
           </ul>
         </div>
       </section>
+      <Footer/>
     </main>
   );
 }
